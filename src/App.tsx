4e7e7992f1131a857330695099cc54cddf7a6f9d
Nastia-rosy <nastiiarosy@gmail.com>
2020-06-08 11:45:28 +0300

    Footer was created without functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HeaderNavigation from './Components/HeaderNavigation/HeaderNavigation';
 import Main from './Components/HeaderNavigation/Main';
+import Footer from './Components/Footer/Footer'
 
 import './styles/App.scss';
 
@@ -8,6 +9,7 @@ const App: React.FC = () => (
   <div className="App">
     <HeaderNavigation />
     <Main />
+    <Footer />
   </div>
 );
 
