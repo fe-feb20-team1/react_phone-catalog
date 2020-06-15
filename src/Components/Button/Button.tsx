ff4303ec433a051af5db0aaccace7c9969b24081
Nastia-rosy <nastiiarosy@gmail.com>
2020-06-16 01:03:02 +0300

    Cards section was created

---
+++ src/Components/Button/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Button.scss';
+
+const Button = () => {
+  return (
+    <>
+      <button className="button">
+        Add to card
+      </button>
+      <button className="button__favourites"></button>
+    </>
+  )
+}
+
+export default Button;
--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -1,10 +1,50 @@
 import React from 'react';
 import './Card.scss';
+import Button from '../Button/Button'
 
-const Card = () => {
+type Props = {
+  good: Goods;
+  width: number;
+}
+
+const Card: React.FC<Props> = ({ good, width }) => {
   return (
-    <>
-    </>
+    <div
+      className="hot-prices__card card"
+      style={{ transform: `translateX(${width}px)` }}
+    >
+      <img src={good.imageUrl} className="card__image" alt={good.name} />
+      <h5 className="card__title">
+        {good.name}
+      </h5>
+      <span className="card__new-price">
+        $799
+      </span>
+      <span className="card__old-price">
+        ${good.price}
+      </span>
+      <div className="card__info">
+        <div className="card__info-title">
+          Screen
+        </div>
+        <div className="card__info-text">
+          {good.screen}
+        </div>
+        <div className="card__info-title">
+          Capacity
+        </div>
+        <div className="card__info-text">
+          {good.capacity}
+        </div>
+        <div className="card__info-title">
+          Ram
+        </div>
+        <div className="card__info-text">
+          {good.ram}
+        </div>
+      </div>
+      <Button />
+    </div>
   )
 }
 
