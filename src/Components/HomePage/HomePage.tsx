ff762294d3e83c9491305e9d8651233b559b19fb
Nastia-rosy <nastiiarosy@gmail.com>
2020-06-16 01:50:19 +0300

    Errors was fixed

--- src/Components/HomePage/HomePage.tsx
+++ src/Components/HomePage/HomePage.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Slider from './Slider/Slider';
 import HotPrices from './HotPrices/HotPrices';
+import Categories from './Categories/Categories';
 
 const HomePage: React.FC = () => {
   return (
     <>
       <Slider />
       <HotPrices />
+      <Categories />
     </>
   );
 };
